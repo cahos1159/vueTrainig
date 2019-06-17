217e22295ad92cde6aab886fdfb2e1cbedd32aa0
cahos1159 <cahos1159@live.fr>
2019-06-17 09:51:20 +0200

    design essai

---
+++ src/components/content/contentB.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="cont">
+    <b-jumbotron  id="jumb" lead="Computer DataBase">
+      <tableauOrdi :ordinateurs="ordinateurs"/>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+import tableauOrdi from "./tableauOrdi";
+
+export default {
+  name: "contentB",
+  components: {
+    tableauOrdi
+  },
+  props: {},
+  data(){
+    return{
+    ordinateurs : [{model:'ESSAI',prix:'300'}],
+    }
+  }
+};
+</script>
+
+<style>
+
+#cont{
+  margin: 3%;
+  box-shadow: 10px 5px 5px grey;
+}
+
+#jumb{
+  background-color: #e2f0f3a4;
+}
+</style>
---
+++ src/components/content/tableauOrdi.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <b-table striped hover :items="items"></b-table>
+  </div>
+</template>
+
+<script>
+  export default {
+      name : 'tableauOrdi',
+      props : ['ordinateurs'],
+    data() {
+      return {
+        items:
+          this.props.ordinateurs
+        
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
